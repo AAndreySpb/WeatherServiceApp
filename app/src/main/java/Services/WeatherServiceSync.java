f5ea81bc3ddc0580d1e67d0de02624f663d74dba
AndreySpb <alekseev@rambler.ru>
2015-05-31 23:56:43 +0400

    Added base class (Service) for both variant of services

--- app/src/main/java/Services/WeatherServiceSync.java
+++ app/src/main/java/Services/WeatherServiceSync.java
@@ -1,7 +1,22 @@
 package Services;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
 /**
  * Created by Адрей on 31.05.2015.
  */
-public class WeatherServiceSync {
+public class WeatherServiceSync extends Service {
+
+    /**
+     * Called when a component calls bindService() with the proper
+     * intent.  Return the concrete implementation of DownloadRequest
+     * cast as an IBinder.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        //@@ TODO here should be right implementation
+        return null;
+    }
 }
